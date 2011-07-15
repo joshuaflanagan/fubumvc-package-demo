61c6f9696f7395660c8a718b809030c94a8b4c55
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-07-15 11:44:22 -0500

    Initial commit

---
+++ source/MyApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+[assembly: AssemblyTitle("MyApplication")]
+[assembly: AssemblyCompany("DarthFubuMVC")]
+[assembly: AssemblyProduct("MyApplication")]
+[assembly: AssemblyCopyright("Copyright © DarthFubuMVC 2011")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
